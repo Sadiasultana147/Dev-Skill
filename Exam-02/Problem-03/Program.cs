64c7f12bf9bc8948b5d4a514b7dc9ec2a18ba05c
Sadiasultana147 <sadiafency.dev@outlook.com>
2021-05-31 21:19:11 +0600

    Task-03

---
+++ Exam-02/Problem-03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problem_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic o = new { firstStatement = "hello", SecondStatement = "world" };
+            Console.WriteLine(o.firstStatement);
+            Console.WriteLine(o.SecondStatement);
+        }
+    }
+}
