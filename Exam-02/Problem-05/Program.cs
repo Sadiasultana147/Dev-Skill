76f1c0ec3269ba986b3d9ef81901ee7837d62cd0
Sadiasultana147 <sadiafency.dev@outlook.com>
2021-05-31 21:23:34 +0600

    Task-05

---
+++ Exam-02/Problem-05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problem_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Ex01(ref x);
+
+            int y = 10;
+            Ex02(in y);
+
+            int a = 0;
+            int b = 0;
+            Ex03(out a, out b);
+        }
+
+        public static void Ex01(ref int a)
+        {
+            a = 20;
+        }
+
+        public static void Ex02(in int a)
+        {
+            
+        }
+
+        public static void Ex03(out int a, out int b)
+        {
+            a = 50;
+            b = 90;
+        }
+    }
+}
