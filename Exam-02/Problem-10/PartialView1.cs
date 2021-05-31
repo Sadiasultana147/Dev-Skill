e8ceba3f33e2618623f85ee9169732492ff59d21
Sadiasultana147 <sadiafency.dev@outlook.com>
2021-05-31 21:56:45 +0600

    Task-10

---
+++ Exam-02/Problem-10/PartialView1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_10
+{
+    public partial class HappyClass
+    {
+       public string TotalTime;
+    }
+}
---
+++ Exam-02/Problem-10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HappyClass HC = new HappyClass();
+            HC.ClassStartingTime = "9.00 pm";
+            HC.ClassEndinTime = "11.00pm";
+            HC.TotalTime = " 2 hours";
+            Console.WriteLine(HC.ClassStartingTime);
+            Console.WriteLine(HC.ClassEndinTime);
+            Console.WriteLine(HC.ClassEndinTime);
+        }
+    }
+}
