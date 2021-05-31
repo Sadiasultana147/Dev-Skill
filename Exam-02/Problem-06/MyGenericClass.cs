467214b3d4bb422e2f40073055cfe34d345139bd
Sadiasultana147 <sadiafency.dev@outlook.com>
2021-05-31 22:11:18 +0600

    Task-06

---
+++ Exam-02/Problem-06/MyGenericClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_06
+{
+    
+    public class MyGenericClass<T, U>
+    {
+        public T a;
+        public U b;
+        
+    }
+}
---
+++ Exam-02/Problem-06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problem_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyGenericClass<String, int> x = new MyGenericClass<string, int>();
+            x.a = "Hello";
+            x.b = 5;
+            Console.WriteLine(x.a);
+
+            Console.WriteLine(x.b);
+
+
+        }
+    }
+}
